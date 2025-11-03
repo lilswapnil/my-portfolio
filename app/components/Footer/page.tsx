b5386950c053ddd98f6f0bb99c25ce4d9d24a21f
Swapnil Satish Bhalerao <ssbhaler@syr.edu>
2025-11-03 13:51:46 -0500

    fixing home margin and padding

--- app/components/Footer/page.tsx
+++ app/components/Footer/page.tsx
@@ -1,6 +1,6 @@
 export default function Footer () {
     return (
-        <footer className="w-full border-1 glass-container text-white py-2">
+        <footer className="w-full border-1 glass-container text-white-50 py-2">
             <div className="container mx-auto text-center">
                 <p>&copy; {new Date().getFullYear()} Scott's Portfolio. All rights reserved.</p>
             </div>
